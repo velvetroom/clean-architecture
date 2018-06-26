813ee31b2bf5320bb9f6711bd277769992087923
Zero <zero.griffin@gmail.com>
2018-06-26 07:36:31 +0200

    Add demo landing screen

--- Demo/Source/View.swift
+++ Demo/Source/View.swift
@@ -1,5 +1,16 @@
 import UIKit
 
 class View:UIViewController {
+    init() {
+        super.init(nibName:nil, bundle:nil)
+        self.configureView()
+    }
     
+    required init?(coder:NSCoder) {
+        return nil
+    }
+    
+    private func configureView() {
+        self.title = NSLocalizedString("View_Title", comment:String())
+    }
 }
